47ac3a0434980fbe25d3fcc799caaf66a2f1565d
screret <68943070+screret@users.noreply.github.com>
2023-11-30 08:50:19 +0200

    update my branch?

--- kubejs/startup_scripts/main_startup.js
+++ kubejs/startup_scripts/main_startup.js
@@ -1,33 +1,12 @@
 // priority 0
 
-// shared with server_scripts for LootJS
-global.blockBanList = [
-  "minecraft:enchanting_table",
-  "minecraft:furnace",
-  "minecraft:brewing_stand",
-  "minecraft:smoker",
-  "minecraft:blast_furnace",
-  "minecraft:campfire",
-  "minecraft:soul_campfire"
-]
-
-// shared with server_scripts for LootJS
-global.blockReplacementMap = {
-  "minecraft:chest": "tfc:wood/chest/oak",
-  "minecraft:trapped_chest": "tfc:wood/trapped_chest/oak", // won't see on jade because it is cheating
-  "minecraft:barrel": "tfc:wood/barrel/oak", // not same functionality but :shrug:
-  "minecraft:lectern": "tfc:wood/lectern/oak",
-  "minecraft:bell": "tfc:brass_bell",
-  "minecraft:torch": "tfc:torch",
-  "minecraft:beehive": "firmalife:beehive"
-}
 
 StartupEvents.registry("item", (event) => {
   registerItems(event)
 })
 
 StartupEvents.registry("block", (event) => {
-  registerGTCOres(event)
+
   registerBlocks(event)
 })
 
@@ -43,16 +22,6 @@ StartupEvents.postInit((event) => {
   if (!Platform.isClientEnvironment()) return
   addTooltipToBlocks(event)
 })
-
-// TODO: Implement this
-/*
-global.hideAndRemoveRecipe = [
-  ["minecraft:torch",{output: "minecraft:torch"}],
-  [],
-  [],
-  [],
-]
-*/
 
 GTCEuStartupEvents.registry("gtceu:recipe_type", (event) => {
   registerRecipeTypes(event)
@@ -88,9 +57,6 @@ GTCEuStartupEvents.registry("gtceu:material", (event) => {
 
 GTCEuStartupEvents.registry("gtceu:material", (event) => {
     registerGTCEuOrganicMaterial(event)
-})
-
-ForgeEvents.onEvent("net.minecraftforge.event.entity.EntityEvent$EnteringSection", event => {
 })
 
 ForgeEvents.onEvent("net.minecraftforge.event.entity.player.ItemTooltipEvent", event => {
